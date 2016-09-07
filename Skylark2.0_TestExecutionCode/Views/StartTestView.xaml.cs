1c0bacd13ac7e2ec7c6012a9612920e8e4833bbd
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2016-09-07 13:51:49 +0700

    Added the standard C# files in the folders.

---
+++ Skylark2.0_TestExecutionCode/Views/StartTestView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Skylark2._0_TestExecutionCode.Views
+{
+    /// <summary>
+    /// Interaction logic for StartTestView.xaml
+    /// </summary>
+    public partial class StartTestView : Window
+    {
+        public StartTestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
